e40e4b994e0c400ef0edff81cae73a2110a9a41d
Eyal Gindi <eyal.gindi@deepinstinct.com>
2023-07-13 09:50:55 +0300

    Keebio Sinc Rev3 - Preliminary Support

--- boards/keebio/sinc/boot.py
+++ boards/keebio/sinc/boot.py
@@ -1,8 +1,8 @@
 
-#device_name = "SINC_R3_R"
-device_name = "SINC_R3_L"
+new_name = "SINC_R3_L"
+#new_name = "SINC_R3_R"
 import storage
 storage.remount("/", readonly=False)
 m = storage.getmount("/")
-m.label = device_name
+m.label = new_name
 storage.remount("/", readonly=True)
--- boards/keebio/sinc/main.py
+++ boards/keebio/sinc/main.py
@@ -6,6 +6,7 @@ from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.split import Split, SplitType, SplitSide
 from kmk.modules.layers import Layers
+<<<<<<< master
 from kmk.modules.holdtap import HoldTap
 <<<<<<< master
 <<<<<<< master
@@ -15,9 +16,11 @@ from kmk.modules.tapdance import TapDance
 =======
 from kmk.modules.tapdance import TapDance
 >>>>>>> TapDance
+=======
+>>>>>>> Keebio Sinc Rev3 - Preliminary Support
 from storage import getmount
-from kmk.extensions.media_keys import MediaKeys
 from kmk.extensions.RGB import RGB, AnimationModes
+<<<<<<< master
 from kmk.modules.encoder import EncoderHandler
 
 <<<<<<< master
@@ -36,6 +39,8 @@ import kc_seq as SQ
 _____ = KC.TRNS
 XXXXXXX = KC.NO
 FnKey = KC.MO(1)
+=======
+>>>>>>> Keebio Sinc Rev3 - Preliminary Support
 
 keyboard = KMKKeyboard()
 <<<<<<< master
@@ -52,6 +57,7 @@ import kc_tap_dance as TD
 =======
 =======
 keyboard.debug_enabled = True
+<<<<<<< master
 >>>>>>> Holdtap to replace LGUI+KC.__ABC__
 
 keyboard.extensions.append(MediaKeys())
@@ -69,25 +75,28 @@ keyboard.debug_enabled = True
 import kc_holdtap as HT
 import kc_tap_dance as TD
 >>>>>>> TapDance
+=======
+keyboard.modules.append(Layers())
+>>>>>>> Keebio Sinc Rev3 - Preliminary Support
 
-# Per side configuration is set based on the device label (end with L or R).
-# uncomment device_name line for the corresponding side on boot.py
 split_side = SplitSide.LEFT if str(getmount('/').label)[-1] == 'L' else SplitSide.RIGHT
+
 split = Split(split_type=SplitType.UART,
               split_side=split_side,
               data_pin=board.GP0,
               data_pin2=board.GP1,
               use_pio=True,
               uart_flip=split_side is SplitSide.RIGHT)
+
 keyboard.modules.append(split)
 
-enc_module = EncoderHandler()
-enc_module.map = [((KC.VOLD, KC.VOLU, KC.MUTE),)]
-keyboard.modules.append(enc_module)
+_______ = KC.TRNS
+XXXXXXX = KC.NO
+FnKey = KC.MO(1)
 
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 keyboard.col_pins = (
-    board.GP29,
+            board.GP29,
             board.GP28,
             board.GP27,
             board.GP7,
@@ -98,22 +107,17 @@ keyboard.col_pins = (
             board.GP13,
 )
 if split_side is SplitSide.RIGHT:
-    enc_module.pins = ((board.GP5, board.GP6, None),)
-    num_rgb_pixels = 56
     keyboard.row_pins = (
-        board.GP8,
+                board.GP8,
                 board.GP9,
-                board.GP16,
+                board.GP16,            
                 board.GP19,
                 board.GP26,
                 board.GP17,
     )
 else:
-    enc_module.pins = ((board.GP21, board.GP20, None),)
-
-    num_rgb_pixels = 57
     keyboard.row_pins = (
-        board.GP26,
+                board.GP26,
                 board.GP25,
                 board.GP19,
                 board.GP24,
@@ -121,11 +125,11 @@ else:
                 board.GP16,
     )
 
-
 keyboard.keymap = [
     [
         KC.MUTE, KC.ESC, KC.F1, KC.F2, KC.F3, KC.F4, KC.F5, KC.F6,              KC.F7, KC.F8, KC.F9, KC.F10, KC.F11, KC.F12, KC.DEL, KC.INS,
         KC.F1, KC.F2, KC.GRV, KC.N1, KC.N2, KC.N3, KC.N4, KC.N5, KC.N6,         KC.N7, KC.N8, KC.N9, KC.N0, KC.MINS, KC.EQL, KC.BSPC, KC.HOME,
+<<<<<<< master
 <<<<<<< master
 <<<<<<< master
 <<<<<<< master
@@ -171,6 +175,12 @@ keyboard.keymap = [
 =======
         _____, _____, _____, _____, _____, KC.LED_TOG(), KC.ENT,                                    KC.ENT, _____, _____, _____, _____, _____, _____
 >>>>>>> TapDance
+=======
+        KC.F3, KC.F4, KC.TAB, KC.Q, KC.W, KC.E, KC.R, KC.T,                     KC.Y, KC.U, KC.I, KC.O, KC.P, KC.LBRC, KC.RBRC, KC.BSLS, KC.END,
+        KC.F5, KC.F6, KC.CAPS, KC.A, KC.S, KC.D, KC.F, KC.G,                    KC.H, KC.J, KC.K, KC.L, KC.SCLN, KC.QUOT, KC.ENT, KC.PGUP,
+        KC.F7, KC.F8, KC.LSFT, KC.Z, KC.X, KC.C, KC.V, KC.B,                    KC.N, KC.M, KC.COMM, KC.DOT, KC.SLSH, KC.RSFT, KC.UP, KC.PGDN,
+        KC.F9, KC.F10, KC.LCTL, KC.LALT, KC.LGUI, KC.MO(1), KC.SPC,     KC.MO(1), KC.SPC, KC.RALT, KC.RCTL, KC.RGUI, KC.LEFT, KC.DOWN, KC.RGHT
+>>>>>>> Keebio Sinc Rev3 - Preliminary Support
     ]
 ]
 
@@ -182,6 +192,7 @@ keyboard.coord_mapping = [0,  2, 3, 4, 5, 6, 7, 8,                 91, 92, 93, 9
                           36, 37, 38, 40, 41, 42, 43, 44,            63, 64, 65, 66, 67, 69, 70, 71,
                           45, 46, 47, 48, 49, 50, 52,                 55, 56, 57, 58, 60, 61, 62]
 
+<<<<<<< master
 
 <<<<<<< master
 <<<<<<< master
@@ -198,7 +209,11 @@ keyboard.coord_mapping = [0,  2, 3, 4, 5, 6, 7, 8,                 91, 92, 93, 9
 # rgb_ext = RGB(pixel_pin=board.NEOPIXEL, num_pixels=num_rgb_pixels)
 >>>>>>> TapDance
 # keyboard.extensions.append(rgb_ext)
+=======
+keyboard.extensions = []
+>>>>>>> Keebio Sinc Rev3 - Preliminary Support
 
 
 if __name__ == '__main__':
-    keyboard.go()    
+    keyboard.go()
+    
